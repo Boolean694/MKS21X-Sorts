d9b1503d86e8eff08c02fd761846583a4011ee9f
Julian Wong <jwong01@stuy.edu>
2018-12-08 00:54:06 -0500

    did i seriously get this to work in 5 mins whaaaaaaat

--- Sorts.java
+++ Sorts.java
@@ -1,7 +1,6 @@
 public class Sorts {
 	public static void selectionsort(int[] a) {
 		if(a.length == 0) {System.out.print("");}
-		
 		int sm = 2147483647;
 		int sml = 0;
 		int plh = 0;
@@ -23,11 +22,34 @@ public class Sorts {
 		s += "]";
 		System.out.println(s);
 	}
-	
+	public static void bubblesort(int[] a) {
+		if(a.length == 0){System.out.println("");}
+		if(a.length == 1){System.out.println("[" + a[0] + "]");}
+		int plh = 0;
+		for(int q = a.length - 1; q > 0; q--) {
+			for(int w = 0; w < q; w++) {
+				if(a[w] > a[w + 1]) {
+					plh = a[w];
+					a[w] = a[w + 1];
+					a[w + 1] = plh;
+					plh = 0;
+				}
+				else{plh += 0;}
+			}
+		}
+		String s = "[";
+		for(int e = 0; e < a.length - 1; e++) {
+			s += a[e];
+			s += ",";
+		}
+		s += a[a.length - 1];
+		s += "]";
+		System.out.println(s);
+	}
 	public static void main(String[] yeeyee) {
 		try {
-			int[] b = {1,2147483647,85,35};
-			selectionsort(b);
+			int[] b = {10,54,2,7,32,645,237,43,56};
+			bubblesort(b);
 		}
 		catch(ArrayIndexOutOfBoundsException e) {System.out.println("Put stuff in your array!");}
 	}
