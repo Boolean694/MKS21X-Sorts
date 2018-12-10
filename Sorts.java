e6546d7fda9d77902964fdf16938f859116c9436
Julian Wong <jwong01@stuy.edu>
2018-12-10 11:02:33 -0500

    start writing insertion

--- Sorts.java
+++ Sorts.java
@@ -1,9 +1,10 @@
 public class Sorts {
 	public static void selectionSort(int[] a) {
-		if(a.length == 0) {System.out.print("");}
+		if(a.length == 0) {System.out.print("[]");}
 		int sm = 2147483647;
 		int sml = 0;
 		int plh = 0;
+		if(a.length != 0) {
 		for(int q = 0; q < a.length - 1; q++) {
 			for(int w = q; w < a.length; w++) {
 				if(a[w] < sm) {sm = a[w]; sml = w;}
@@ -13,19 +14,21 @@ public class Sorts {
 			a[sml] = plh;
 			sml = 0; sm = 2147483647; plh = 0;
 		}
-		String s = "[";
+		}
+		/*String s = "[";
 		for(int e = 0; e < a.length - 1; e++) {
 			s += a[e];
 			s += ",";
 		}
 		s += a[a.length - 1];
 		s += "]";
-		System.out.println(s);
+		System.out.println(s);*/
 	}
 	public static void bubbleSort(int[] a) {
-		if(a.length == 0){System.out.println("");}
+		if(a.length == 0){System.out.println("[]");}
 		if(a.length == 1){System.out.println("[" + a[0] + "]");}
 		int plh = 0;
+		if(a.length != 0) {
 		for(int q = a.length - 1; q > 0; q--) {
 			for(int w = 0; w < q; w++) {
 				if(a[w] > a[w + 1]) {
@@ -37,13 +40,24 @@ public class Sorts {
 				else{plh += 0;}
 			}
 		}
-		String s = "[";
+		}
+		/*String s = "[";
 		for(int e = 0; e < a.length - 1; e++) {
 			s += a[e];
 			s += ",";
 		}
 		s += a[a.length - 1];
 		s += "]";
-		System.out.println(s);
+		System.out.println(s);*/
+	}
+	public static void insertionSort(int[] a) {
+	    if(a.length == 0) {System.out.println("[]");}
+	    if(a.length == 1) {System.out.println("[" + a[0] + "]");}
+	    int plh = 0;
+	    if(a.length != 0) {
+	    for(int q = 1; q < a.length; q++)  {
+	        
+	    }
+	    }
 	}
 }
